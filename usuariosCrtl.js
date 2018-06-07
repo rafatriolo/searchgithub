e167ca59e2d5ab7ac669833b7b9a0259a78c67aa
Rafael <rafatriolo@gmail.com>
2018-06-07 14:13:40 -0300

    search github

--- usuariosCrtl.js
+++ usuariosCrtl.js
@@ -1,5 +1,6 @@
 usuarios.controller('usuarios_lista', function($scope, UsuariosSrv){
     $scope.get = function(i){
+        $scope.loading = true;
         UsuariosSrv.RetornoUsuarios.get({username: i}).$promise.then(function(data){
             $scope.submitted = true;
             $scope.Notfound = false;
@@ -7,6 +8,8 @@ usuarios.controller('usuarios_lista', function($scope, UsuariosSrv){
         }, function(error){
             $scope.submitted = false;
             $scope.Notfound = true;
+        }).finally(function(){
+            $scope.loading = false;
         });
 
         UsuariosSrv.RetornoUsuarios.query({teste: i}).$promise.then(function(data){
